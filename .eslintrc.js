be2bcbd4a2512541ce458034cf690f360618903d
unknown <vishaliscoding27@gmail.com>
2023-10-04 18:19:29 +0530

    first-commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: [
+    "@react-native",
+    "plugin:storybook/recommended",
+    "plugin:storybook/recommended"
+  ],
+};
---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  stories: ['../components/**/*.stories.?(ts|tsx|js|jsx)'],
+
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-react-native-web',
+  ],
+
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  }
+};
